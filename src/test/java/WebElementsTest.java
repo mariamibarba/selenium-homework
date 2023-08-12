ff33ea193dfd80324704cdfce741e10c663b77ce
mariamibarba <73659597+mariamibarba@users.noreply.github.com>
2023-08-12 09:48:42 +0400

    change Webelement class

--- src/test/java/WebElementsTest.java
+++ src/test/java/WebElementsTest.java
@@ -1,38 +1,26 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-
+import org.testng.annotations.Test;
 import java.util.List;
-
-
 public class WebElementsTest {
     public static WebDriver driver;
 
     @BeforeMethod
     public void setUp() {
-
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-
     }
-
 
     @Test
     public void testWebElementActions() throws InterruptedException {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
         String url = "http://the-internet.herokuapp.com/add_remove_elements/";
         driver.get(url);
-
 
         for (int i = 0; i < 3; i++) {
             WebElement addElementButton = driver.findElement(By.xpath("//*[@id=\"content\"]/div/button"));
@@ -47,48 +35,34 @@ public class WebElementsTest {
         System.out.println("Last Delete Button (findElement()): " + lastDeleteButton);
 
         List<WebElement> deleteButton = driver.findElements(By.cssSelector("[class^='added']"));
-        WebElement lastDeleteButtonCss = deleteButton.get(deleteButton.size() - 1);
-        System.out.println("Last Delete button (findElements() with cssSelector): " + lastDeleteButtonCss);
-
+        WebElement element = deleteButton.get(deleteButton.size() - 1);
+        System.out.println("Last Delete button (findElements() with cssSelector): " + element.getText());
 
         Thread.sleep(2000);
         // Print the last 'Delete' button using findElement() and relative XPath
         WebElement lastDeleteButtonXPath = driver.findElement(By.xpath("//button[contains(@class, 'manually') and text()='Delete']"));
         System.out.println("Last 'Delete' button (findElement() - XPath): " + lastDeleteButtonXPath.getText());
-        driver.quit();
-
-
-
     }
-    @Test
-    public void test2() throws InterruptedException {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        String url= "http://the-internet.herokuapp.com/challenging_dom";
-        driver.get(url);
 
+    @Test
+    public void testChallengingDom() throws InterruptedException {
+
+        driver.navigate().to("http://the-internet.herokuapp.com/challenging_dom");
 
         WebElement elementWithApeirian9 = driver.findElement(By.xpath("//td[text()='Apeirian9']/following-sibling::td"));
         System.out.println("Lorem value of element with Ipsum value 'Apeirian9': " + elementWithApeirian9.getText());
-        Thread.sleep(1000);
-
+        Thread.sleep(2000);
 
         WebElement nextElement = elementWithApeirian9.findElement(By.xpath("following-sibling::td"));
         System.out.println("Next element of element with Ipsum value 'Apeirian9': " + nextElement.getText());
-        Thread.sleep(1000);
-
-
-        driver.quit();
-
-
     }
+
     @AfterMethod
-    public void after(){
-        driver.quit();
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
-
-
-
 
 }
+
